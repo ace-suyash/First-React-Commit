6475a83d4fc25c0607bd0c6145a04743c6bad731
ace-suyash <ace.suyash07@gmail.com>
2026-03-27 12:26:09 +0530

    Comment

--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -10,6 +10,10 @@ const App = () => {
 
   localStorage.setItem('user', JSON.stringify(user))
   const usera = JSON.parse(localStorage.getItem('user'))
+  
+  //We have to pass the object in the string form always 
+  // , so JSON.STRINGIFY IS USED IN ORDER TO CONVERT TH EOBJECT user in STRING
+ 
 
   console.log(usera);
   return (
